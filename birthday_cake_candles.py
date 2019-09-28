860006c109f7a795e7ab7015782f3aba8723a3ac
Mohit-1106 <yogeshyadav80666@gmail.com>
2019-09-28 22:00:42 +0530

    remove

--- birthday_cake_candles.py
+++ birthday_cake_candles.py
@@ -1,3 +1,4 @@
+
 #!/bin/python3
 
 import math
@@ -22,6 +23,6 @@ def birthdayCakeCandles(ar):
 ar_count = int(input())
 
 ar = list(map(int, input().rstrip().split()))
-# Calling the function birthdayCakeCandles and storing it's return value on result
+# Calling function birthdayCakeCandles and storing it's return value on result
 result = birthdayCakeCandles(ar)
 print(result)
